11e0f5fd93a006a5107e722b69bfc5f311203bf7
Kyle Coburn <kylecoburn@gmail.com>
2019-12-28 17:34:58 -0800

    🐛🔬 Cast to ColorPalette

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -1,4 +1,5 @@
 import tailwindColorPalette from '../dist'
+import { ColorPalette } from '../types'
 
 describe('parameters', () => {
 	it('requires a valid color', () => {
@@ -33,21 +34,21 @@ describe('options', () => {
 	})
 	it('ui', () => {
 		const colors = tailwindColorPalette('red', { ui: true })
-		expect(colors['info']['500']).toEqual('#a0c1cc')
+		expect((colors['info'] as ColorPalette)['500']).toEqual('#a0c1cc')
 		expect(colors).not.toHaveProperty('black')
 	})
 	it('uiMix', () => {
 		const colors = tailwindColorPalette('red', { ui: true, uiMix: 0.5 })
-		expect(colors['info']['500']).toEqual('#da9383')
+		expect((colors['info'] as ColorPalette)['500']).toEqual('#da9383')
 	})
 	it('grayscale', () => {
 		const colors = tailwindColorPalette('red', { grayscale: true })
-		expect(colors['gray']['500']).toEqual('#b2aaa8')
+		expect((colors['gray'] as ColorPalette)['500']).toEqual('#b2aaa8')
 		expect(colors).not.toHaveProperty('cta')
 	})
 	it('grayscaleMix', () => {
 		const colors = tailwindColorPalette('red', { grayscale: true, grayscaleMix: 0.5 })
-		expect(colors['gray']['500']).toEqual('#e4775c')
+		expect((colors['gray'] as ColorPalette)['500']).toEqual('#e4775c')
 	})
 	it('palette', () => {
 		const colors = tailwindColorPalette('red', { palette: { custom: '#123' } })
@@ -55,18 +56,21 @@ describe('options', () => {
 	})
 	it('colorScale', () => {
 		const colors = tailwindColorPalette('red', { colorscale: ["lighter", "light", "medium", "dark", "darker"] })
-		expect(colors['brand']['lighter']).toEqual('#ffcccc')
-		expect(colors['brand']['light']).toEqual('#ff6666')
+		const brandColors = colors['brand'] as ColorPalette
+		expect(brandColors['lighter']).toEqual('#ffcccc')
+		expect(brandColors['light']).toEqual('#ff6666')
 	})
 	it('colorScaleNumeric', () => {
 		const colors = tailwindColorPalette('red', { colorscale: [1,2,3,4,5] })
-		expect(colors['brand']['1']).toEqual('#ffcccc')
-		expect(colors['brand']['2']).toEqual('#ff6666')
+		const brandColors = colors['brand'] as ColorPalette
+		expect(brandColors['1']).toEqual('#ffcccc')
+		expect(brandColors['2']).toEqual('#ff6666')
 	})
 	it('colorScaleUiGray', () => {
 		const colors = tailwindColorPalette('blue', { colorscale: ["light", "medium", "dark"], grayscale: true, ui: true })
-		expect(colors['brand']['light']).toEqual('#ccccff')
-		expect(colors['gray']['medium']).toEqual('#aca9b0')
-		expect(colors['danger']['dark']).toEqual('#2f0615')
+		const brandColors = colors['brand'] as ColorPalette
+		expect((brandColors as ColorPalette)['light']).toEqual('#ccccff')
+		expect((colors['gray'] as ColorPalette)['medium']).toEqual('#aca9b0')
+		expect((colors['danger'] as ColorPalette)['dark']).toEqual('#2f0615')
 	})
 })
